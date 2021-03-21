7e9986c6ae696ded9bc7ec317d783ab82d81e52d
Jonathan Rafael Guzman <guzman.jonathan.r@gmail.com>
2021-03-21 10:56:54 -0400

    updated permissions to user profile feed api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models 
@@ -120,7 +121,9 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     authentication_classes = (TokenAuthentication,)
     serializer_class = serializers.ProfileFeedItemSerializer
     queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdatOwnStatus, IsAuthenticated)
 
     def perform_create(self, serializer):
         """Sets the user profile to be the logged in user"""
         serializer.save(user_profile=self.request.user)
+
